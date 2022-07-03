6a61ae9efdafc1b429e2b013dc2ab249ee682739
Israel Guzman <israel.guzman@usach.cl>
2022-07-03 02:41:41 -0400

    Sección critica de escritura
    Printf de visibilidad, empieza del 2 y termina en 101 así que está bien lee 100

--- lab2.c
+++ lab2.c
@@ -18,6 +18,16 @@ typedef struct visibilidad
     long double ruido;
 } vis;
 
+typedef struct Disco
+{
+    long double u;
+    long double v;
+    long double real;
+    long double img;
+    long double ruido;
+    int contadorVis;
+} Disk;
+
 FILE * flujo;
 pthread_mutex_t mutexLectura;
 pthread_mutex_t mutexEscritura;
@@ -25,6 +35,7 @@ int ancho_disco, cant_discos, chunk;
 int contador = 0; //cuenta visibilidades
 int contadorH = 0; //cuenta hebras
 int leido = 0;
+Disk* Discos;
 /*
 entrada:
 salida:
@@ -59,7 +70,7 @@ int hashDisk(int ancho_disco, int cant_discos, long double distancia){
         {
             if (distancia >= limInf && distancia < limSup  )
             {
-                return i+1;
+                return i;
             }            
             else{
                 limInf = ancho_disco;
@@ -69,14 +80,14 @@ int hashDisk(int ancho_disco, int cant_discos, long double distancia){
         else if(i == cant_discos-1){
             if (distancia >= limInf)
             {
-                return i+1;
+                return i;
             }
             
         }
         else{
             if (distancia >= limInf && distancia < limSup)
             {
-                return i+1;
+                return i;
             }
             else{
                 limInf = limInf + ancho_disco;
@@ -115,17 +126,30 @@ void * thread_rutine(void *unused){
                     long double distancia =  sqrt((auxVis.real * auxVis.real) + (auxVis.img * auxVis.img));
 
                     //asignación de disco
-                    int disco = hashDisk(ancho_disco, cant_discos, distancia);
+                    int discoID = hashDisk(ancho_disco, cant_discos, distancia);
                     
                     chunkLeido++;
                     contador++;
                     //Print de control
-                    printf("Soy la visibilidad %d\n Distancia: %LF \n Disco: %d\n", contador, distancia, disco);
+                    printf("Soy la visibilidad %d\n Distancia: %LF \n Disco: %d\n", contador+1, distancia, discoID);
                     printf("%Lf,%Lf,%Lf,%Lf,%Lf\n\n\n", auxVis.u, auxVis.v, auxVis.real, auxVis.img, auxVis.ruido);
                     
                     //escritura de archivos
                     // que nos falta
                     // inicializar el arreglo de struct que contendra la información de cada disco
+                    // ------------------------ ACTUALIZAR PROPIEDADES ------------------------------
+                    pthread_mutex_lock(&mutexEscritura);
+                    Discos[discoID].u = Discos[discoID].u + auxVis.u;
+                    Discos[discoID].v = Discos[discoID].v + auxVis.v;
+                    Discos[discoID].real = Discos[discoID].real + auxVis.real;
+                    Discos[discoID].img = Discos[discoID].img + auxVis.img;
+                    Discos[discoID].ruido = Discos[discoID].ruido + auxVis.ruido;
+                    Discos[discoID].contadorVis = Discos[discoID].contadorVis + 1;
+
+                    printf("Actualizar Disco %d\n", discoID);
+                    printf("%Lf,%Lf,%Lf,%Lf,%Lf\n Cant: %d\n\n", Discos[discoID].u, Discos[discoID].v, Discos[discoID].real, Discos[discoID].img, Discos[discoID].ruido,Discos[discoID].contadorVis);
+                    
+                    pthread_mutex_unlock(&mutexEscritura);
                 }
                 else{
                     leido = 1;
@@ -230,8 +254,18 @@ int main(int argc, char** argv){
 
     //----------------------- creacion y declaracion de threads --------------------    
     
-    
+    Discos = (Disk*)malloc(sizeof(Disk)*cant_discos);
 
+    for (int i = 0; i < cant_discos; i++)
+    {
+        Discos[i].u = 0;
+        Discos[i].v = 0;
+        Discos[i].real = 0;
+        Discos[i].img = 0;
+        Discos[i].ruido = 0;
+        Discos[i].contadorVis = 0;
+    }
+    
     //Inicialización de mutex    
     pthread_mutex_init(&mutexLectura, NULL);
     pthread_mutex_init(&mutexEscritura, NULL);
@@ -253,7 +287,7 @@ int main(int argc, char** argv){
     //escritura de archivo 
 
 
-    
+
     //destroy mutex
     pthread_mutex_destroy(&mutexLectura);
     pthread_mutex_destroy(&mutexEscritura);
